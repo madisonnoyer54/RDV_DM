3bba226e5bbf3cdcd4a284056a81488e9605000d
madisonnoyer54 <115881298+madisonnoyer54@users.noreply.github.com>
2024-03-11 21:25:42 +0100

    image
    ajout de la bibliotheque pour l'image + commencement du code

--- tinykaboom.cpp
+++ tinykaboom.cpp
@@ -6,36 +6,27 @@
 #include <fstream>
 #include <vector>
 
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "stb_image_write.h"
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+
 #include "geometry.h"
 #include "models/Sphere.h"
 #include "models/RoundedCone.h"
 #include "models/Cylinder.h"
 
+int envmap_width, envmap_height;
+std::vector<Vec3f> envmap;
+
 const float sphere1_radius   = 0.5; // Rayon pour la petite boule
 const float sphere2_radius  = 0.6; // Rayon pour la boule moyenne
 const float sphere3_radius   = 0.7; // Rayon pour la grande boule
 
-int main() {
 
-    //Initialisation des objets
-    Sphere boule1 = Sphere(Vec3f(0, 0.7, 0), sphere1_radius);
-    Sphere boule2 = Sphere(Vec3f(0, 0, 0), sphere2_radius);
-    Sphere boule3 = Sphere(Vec3f(0, -0.7, 0), sphere3_radius);
 
-    RoundedCone carrote = RoundedCone(Vec3f(0, 0.7, 0.4), Vec3f(0, 0.7, 0.8), 0.04, 0.1, Vec3f(0.8,0.6,0));
-
-    Cylinder bras1 = Cylinder(Vec3f(0, 0, 0), Vec3f(-1.2, 0.45f, 0), 0.03, Vec3f(0.5, 0.4, 0));
-    Cylinder bras2 = Cylinder(Vec3f(-1.2, 0.45f, 0), Vec3f(-1.35, 0.6f, 0), 0.03,Vec3f(0.5, 0.4, 0));
-
-    Model ** models = new Model*[6]{
-        &boule1,
-        &boule2,
-        &boule3,
-        &carrote,
-        &bras1,
-        &bras2
-    };
-
+void render(Model** models) {
+    
 	const int   width    = 960;     // image width
 	const int   height   = 720;     // image height
 	const float fov      = M_PI/3.; // field of view angle
@@ -47,6 +38,8 @@ int main() {
 		    float dir_x =  (i + 0.5) -  width/2.;
 		    float dir_y = -(j + 0.5) + height/2.;    // this flips the image at the same time
 		    float dir_z = -height/(2.*tan(fov/2.));
+
+            // avoir
 		    Vec3f hit;
 
             Vec3f pos = Vec3f(0, 0, 3);
@@ -81,6 +74,45 @@ int main() {
 		}
 	}
 	ofs.close();
+}
+
+int main() {
+    
+    int n = -1;
+
+    unsigned char *pixmap = stbi_load("../envmap.jpg", &envmap_width, &envmap_height, &n, 0);
+    if (!pixmap || 3!=n) {
+        std::cerr << "Error: can not load the environment map" << std::endl;
+        return -1;
+    }
+    envmap = std::vector<Vec3f>(envmap_width*envmap_height);
+    for (int j = envmap_height-1; j>=0 ; j--) {
+        for (int i = 0; i<envmap_width; i++) {
+            envmap[i+j*envmap_width] = Vec3f(pixmap[(i+j*envmap_width)*3+0], pixmap[(i+j*envmap_width)*3+1], pixmap[(i+j*envmap_width)*3+2])*(1/255.);
+        }
+    }
+    stbi_image_free(pixmap);
+
+    //Initialisation des objets
+    Sphere boule1 = Sphere(Vec3f(0, 0.7, 0), sphere1_radius);
+    Sphere boule2 = Sphere(Vec3f(0, 0, 0), sphere2_radius);
+    Sphere boule3 = Sphere(Vec3f(0, -0.7, 0), sphere3_radius);
+
+    RoundedCone carrote = RoundedCone(Vec3f(0, 0.7, 0.4), Vec3f(0, 0.7, 0.8), 0.04, 0.1, Vec3f(0.8,0.6,0));
+
+    Cylinder bras1 = Cylinder(Vec3f(0, 0, 0), Vec3f(-1.2, 0.45f, 0), 0.03, Vec3f(0.5, 0.4, 0));
+    Cylinder bras2 = Cylinder(Vec3f(-1.2, 0.45f, 0), Vec3f(-1.35, 0.6f, 0), 0.03,Vec3f(0.5, 0.4, 0));
+
+    Model ** models = new Model*[6]{
+        &boule1,
+        &boule2,
+        &boule3,
+        &carrote,
+        &bras1,
+        &bras2
+    };
+
+    render(models);
 
     return 0;
 }
